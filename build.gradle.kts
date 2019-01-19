7aead39c1146f724f5510cbc69879caf299dc27c
Raman Gupta <rocketraman@gmail.com>
2019-01-19 00:52:16 -0500

    Add maven publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   kotlin("jvm") version "1.3.11"
   id("java-gradle-plugin")
+  id("maven-publish")
   id("com.gradle.plugin-publish").version("0.10.0")
 }
 
